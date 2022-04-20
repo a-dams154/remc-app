dd787c2d0a2976e0cc6dc3cf0d269a8f61587175
Tony George <52209091+a-dams154@users.noreply.github.com>
2022-04-20 22:53:20 +0530

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -19,37 +19,6 @@ require("dotenv").config();
 
 //storage
 
-const profile = {
-    postdata: async (req, res, next) => {
-        try {
-            if(req.file){
-                
-                User.image = req.file.path
-            }
-            // let image = req.files.image;
-            // image.mv(`./public/uploads/${Date.now()}.jpg`, (err, done) => {
-            //     if(err) {
-            //         console.log(err);
-            //     } else {
-            //         console.log(done);
-            //         console.log("image uploaded");
-            //     }
-            // });
-            // const oldUser = await Profile.findOne({ email })
-            // if (oldUser) {
-            //     return res.status(409).send("this user is already exist")
-            // }
-
-              
-              
-
-
-
-
-        } catch (err) {
-            console.log(err)
-        }
-    }
+exports.profile = (req,res) => {
+   console.log(req.file)
 }
-
-module.exports = profile;
